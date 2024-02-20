31609557b462cc224ed57d0e4104d4540caab940
Aviram Eldar <107575552+avirameldar@users.noreply.github.com>
2024-02-20 23:13:02 +0200

    done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  redirect,
+} from "react-router-dom";
+import { DarkModeProvider } from "./providers/DarkModeProvider";
+import Login from "./Users/pages/Login";
+import Dashboard from "./screens/Dashboard";
+import SignUp from "./Users/pages/SignUp";
+import "./css/App.css";
+import AboutPage from "./pages/aboutPage/AboutPage";
+import WelcomePage from "./pages/welcomePage/WelcomePage";
+import Footer from "./layout/footer/Footer";
+import SessionTimeout from "./components/SessionTimeout";
+import Paths from "./utils/Paths";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <WelcomePage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+
+    {
+      path: "/sign-up",
+      element: <SignUp />,
+    },
+    {
+      path: "home",
+      element: <Dashboard />,
+    },
+    {
+      path: "about",
+      element: <AboutPage />,
+    },
+  ]);
+
+  React.useEffect(() => {
+    redirect(Paths.ROOT);
+  }, []);
+  return (
+    <div className="body">
+      <DarkModeProvider>
+        <RouterProvider router={router} />
+        <SessionTimeout />
+        <Footer />
+      </DarkModeProvider>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/utils/Paths.js
@@ -0,0 +1,16 @@
+export default {
+  HOME: "/home",
+  ABOUT: "/about",
+  LOGIN: "/login",
+  SIGN_UP: "/sign-up",
+  ROOT: "/",
+  ERROR_PAGE: "*",
+
+  // SIGNUP: "/signup",
+  // USER_PROFILE: "/user-info",
+  // EDIT_USER: "/edit-user",
+  // PREMIUM: "/premiumUser",
+  // TASK_PREMIUM: "/the-task-premium",
+  // LOGOUT: "/logout",
+  // TEST: "/test",
+};
